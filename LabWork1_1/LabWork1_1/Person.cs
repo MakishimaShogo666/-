c3a73641feb627751501e9da93564bf1b0de6ca5
Aleksandr Zhuykov <zhuykov_aleksandr@mail.ru>
2020-12-21 13:21:16 +0700

    style (Person): добавлены комментарии к коду
    - добавлены комментарии ко всем методам и функциям в классе Person

--- LabWork1_1/LabWork1_1/Person.cs
+++ LabWork1_1/LabWork1_1/Person.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Web.RegularExpressions;
 using System.Text.RegularExpressions;
-//enum Person_Gender : bool
-//{
-//    male = true,
-//    female = false,
-//};
+
+/// <summary>
+/// Перечисление полов, nd - not defined (неопределённый пол)
+/// </summary>
 public enum GenderList { мужской, женский, nd }
 
+/// <summary>
+/// Класс Person
+/// </summary>
 public class Person
 {
-    public string Surname;
-    public string Name;
-    public int Age;
-    public GenderList Gender;
+    // Создание полей класса Person
+    //
+    public string Surname; // Фамилия
+    public string Name; // Имя
+    public int Age; // Возраст
+    public GenderList Gender; // Пол
 
+    // Конструктор класса Person по умолчанию
+    //
     public Person()
     {
         Surname = "Нет данных";
@@ -23,6 +29,8 @@ public class Person
         Gender = GenderList.nd;
     }
 
+    // Конструктор класса Person для создания персоны вручную
+    //
     public Person(string SurnameInput, string NameInput, int AgeInput, GenderList GenderInput)
     {
         Surname = SurnameInput;
@@ -31,25 +39,31 @@ public class Person
         Gender = GenderInput;
     }
 
+    // Конструктор класса Person для создания случайной персоны заданного пола с указанным предпочтительным возрастом (выбирается случайный)
+    //
     public Person(int average_age, GenderList GenderInput)
     {
         Random RandomNumber = new Random();
-        Age = RandomNumber.Next(average_age * 2);
+        Age = RandomNumber.Next(average_age/2 + 1, average_age * 2);
         Gender = GenderInput;
-
+        
+        // Выбор фамилии и имени на основе заданного пола
+        //
         switch (Gender)
         {
             case GenderList.мужской:
-                Surname = PersonLibrary.StandardMaleSurnameLibrary[RandomNumber.Next(PersonLibrary.StandardMaleSurnameCount)];
-                Name = PersonLibrary.StandardMaleNameLibrary[RandomNumber.Next(PersonLibrary.StandardMaleNameCount)];
+                Surname = PersonLibrary.StandardMaleSurnameLibrary[RandomNumber.Next(PersonLibrary.StandardMaleSurnameCount)]; // Выбор фамилии из библиотеки мужских фамилий
+                Name = PersonLibrary.StandardMaleNameLibrary[RandomNumber.Next(PersonLibrary.StandardMaleNameCount)]; // Выбор имени из библиотеки мужских имён
                 break;
             case GenderList.женский:
-                Surname = PersonLibrary.StandardFemaleSurnameLibrary[RandomNumber.Next(PersonLibrary.StandardFemaleSurnameCount)];
-                Name = PersonLibrary.StandardFemaleNameLibrary[RandomNumber.Next(PersonLibrary.StandardFemaleNameCount)];
+                Surname = PersonLibrary.StandardFemaleSurnameLibrary[RandomNumber.Next(PersonLibrary.StandardFemaleSurnameCount)]; // Выбор фамилии из библиотеки женских фамилий
+                Name = PersonLibrary.StandardFemaleNameLibrary[RandomNumber.Next(PersonLibrary.StandardFemaleNameCount)]; // Выбор имени из библиотеки женских имён
                 break;
         }
     }
 
+    // Конструктор класса Person для создания случайной персоны заданного возраста
+    //
     public Person(int AgeInput)
     {
         Random RandomNumber = new Random();
@@ -69,6 +83,8 @@ public class Person
         }
     }
 
+    // Метод PersonWrite для записи информации о персоне в консоль
+    //
     public static void PersonWrite(Person person)
     {
         Console.WriteLine(PersonTemplate.SurnameOutputTemplate + person.Surname);
@@ -77,47 +93,58 @@ public class Person
         Console.WriteLine(PersonTemplate.GenderOutputTemplate + person.Gender);
     }
 
+    // Функция InputDigit для ввода цифр в консоль с предлагаемым пользователю условием
+    //
     public static int InputDigit(string condition) 
     {
-        string InputString = "";
-        string DigitPattern = @"[0-9]";
+        string InputString = ""; // Инициализация переменной для записи в неё введённых в консоль значений
+        string DigitPattern = @"[0-9]"; // Шаблон для ввода цифр
         Console.Write(condition);
 
+        // Цикл выполняется до тех пор, пока пользователь не введёт клавишу Enter
+        //
         while (true)
         {
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-            
-            if(((Regex.IsMatch(keyInfo.KeyChar.ToString(), DigitPattern) == true)
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true); // Объясвление и инициализация переменной, соответствующей нажатой клавише без отображения в консоли (true)
+
+            // Если или введена цифра и длина строки в консоли меньше 3-х символов, или нажата клавиша Backspace,
+            // то данные клавиши вводятся в консоль
+            //
+            if (((Regex.IsMatch(keyInfo.KeyChar.ToString(), DigitPattern) == true)
                 &(InputString.Length < 3)) | (keyInfo.Key == ConsoleKey.Backspace))
             {
-                if (keyInfo.Key != ConsoleKey.Backspace)
+                if (keyInfo.Key != ConsoleKey.Backspace) // Если введённая клавиша не BackSpace, то осуществляется ввод в консоль и переменную InputString
                 {
                     InputString = InputString + keyInfo.KeyChar;
-                    if (InputString != "0")
+                    if (InputString != "0") // Если переменная InputString не содержит только 0, то вводится значение с клавиатуры
                     {
                         Console.Write(keyInfo.KeyChar);
                     }
-                    else
+                    else // Иначе в консоль и переменную ничего не записывается
                     {
                         InputString = "";
                         Console.Write("");
                     }
                 }
-                else
+                else // Иначе удаляется предыдущий символ
                 {
-                    if (InputString != "")
+                    if (InputString != "") // Если переменная InputString не пустая, то удаляется один символ
                     {
-                        Console.Write("\b \b");
-                        InputString = InputString.Substring(0, InputString.Length - 1);
+                        Console.Write("\b \b"); // Удаление последнего символа из консоли
+                        InputString = InputString.Substring(0, InputString.Length - 1); // Удаление последнего символа из InputString
                     }
                 }
             }
-            if (keyInfo.Key == ConsoleKey.Enter)
+            if (keyInfo.Key == ConsoleKey.Enter) // Если нажата клавиша Enter, то ввод цифр завершён
             {
                 break;
             }
         }
+
         Console.WriteLine();
+        
+        // Обработка исключения на случай ввода пустой строки
+        //
         try
         {
             return Int32.Parse(InputString);
@@ -129,16 +156,28 @@ public class Person
         }
     }
 
+    /// <summary>
+    /// Функция InputGender для ввода определённых символов в консоль с предлагаемым пользователю условием
+    /// </summary>
+    /// <param name="condition"> - Условие, предъявляемое пользователю</param>
+    /// <returns> 
+    /// Возвращается пол персоны 
+    /// </returns>
     public static string InputGender(string condition)
     {
-        string InputString = "";
-        string GenderPattern = @"[mfMFмжМЖ]";
+        string InputString = ""; // Инициализация переменной для записи в неё введённых в консоль значений
+        string GenderPattern = @"[mfMFмжМЖ]"; // Шаблон для ввода цифр
         Console.Write(condition);
 
         while (true)
         {
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);         
-            if(((Regex.IsMatch(keyInfo.KeyChar.ToString(),GenderPattern)==true) &(InputString.Length < 1))|(keyInfo.Key == ConsoleKey.Backspace))
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            // Если или введены соответствующие шаблону цифры и длина строки в консоли меньше 1-го символа,
+            // или нажата клавиша Backspace, то данные клавиши вводятся в консоль
+            // (остальное - аналогично InputDigit())
+            //
+            if (((Regex.IsMatch(keyInfo.KeyChar.ToString(),GenderPattern)==true) &(InputString.Length < 1))|(keyInfo.Key == ConsoleKey.Backspace))
             {
                 if (keyInfo.Key != ConsoleKey.Backspace)
                 {
@@ -170,36 +209,50 @@ public class Person
         Console.WriteLine();
         return InputString;
     }
+
+    /// <summary>
+    /// Функция InputName для ввода определённых символов в консоль с предлагаемым пользователю условием
+    /// </summary>
+    /// <param name="condition"> - Условие, предъявляемое пользователю </param>
+    /// <returns> 
+    /// Строка с фамилией или именем 
+    /// </returns>
     public static string InputName(string condition)
     {
-        string InputString = "";
-        string NamePattern = @"([a-zA-Z]|[а-яА-Я]|[ -])";
-        string NameException = @"(- |  | -|--)";
-        char[] Delimiters = new char[] { ' ', '-' };
+        string InputString = ""; // Инициализация переменной для записи в неё введённых в консоль значений
+        string NamePattern = @"([a-zA-Z]|[а-яА-Я]|[ -])"; // Шаблон для ввода текста
+        string NameException = @"(- |  | -|--)"; // Шаблон для исключения повторения разделителей слов
+        char[] Delimiters = new char[] { ' ', '-' }; // Массив символов разделителей слов
 
         Console.Write(condition);
 
         while (true)
         {
             ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            // Если введены соответствующие шаблону цифры или нажата клавиша Backspace, то данные клавиши вводятся в консоль
+            //
             if ((Regex.IsMatch(keyInfo.KeyChar.ToString(), NamePattern) == true) | (keyInfo.Key == ConsoleKey.Backspace))
             {
-                if (keyInfo.Key != ConsoleKey.Backspace)
+                if (keyInfo.Key != ConsoleKey.Backspace) // Если нажатая клавиша не Backspace, то данные вводятся в InputString и консоль
                 {
                     InputString = InputString + keyInfo.KeyChar;
+
+                    // Если введён пробел или дефис после пробела или дефиса, то данные не заисываются
+                    //
                     if (Regex.IsMatch(InputString, NameException) == true)
                     {
                         InputString = InputString.Substring(0, InputString.Length - 1);
                         Console.Write("");
                     }
-                    else
+                    else // Иначе - записываются
                     {
                         Console.Write(keyInfo.KeyChar);
                     }
                 }
-                else
+                else // Иначе - удаляется последний символ из консоли и InputString
                 {
-                    if ((InputString != "")&(InputString.Length!=0))
+                    if ((InputString != "")&(InputString.Length!=0)) // Если InputString - не пустая строка, то последний символ удаляется
                     {
                         Console.Write("\b \b");
                         InputString = InputString.Substring(0, InputString.Length - 1);
@@ -212,28 +265,41 @@ public class Person
             }
         }
         Console.WriteLine();
-        InputString = RegisterChanger(InputString, Delimiters);
+        InputString = RegisterChanger(InputString, Delimiters); // Изменение регистра введённых данных
         return InputString;
     }
 
+    /// <summary>
+    /// Функция PersonRead для ввода данных о персоне из консоли
+    /// </summary>
+    /// <returns>
+    /// Возвращается персона с заданными параметрами
+    /// </returns>
     public static Person PersonRead()
     {
-        Person Person = new Person();      
+        Person Person = new Person(); // Создание экземпляра класса Person конструктором по умолчанию
+        
+        // Задание параметров персоны через консоль
+        //
         Person.Surname = InputName(PersonTemplate.SurnameInputTemplate);
         Person.Name = InputName(PersonTemplate.NameInputTemplate);
         Person.Age = 0;
         Person.Age = InputDigit(PersonTemplate.AgeInputTemplate);
-
+        
+        // Если введённый возраст больше максимально возможного (118), то необходимо ввести возраст заново
+        //
         while (Person.Age> PersonLibrary.MaxAge)
         {
             Console.WriteLine($"Введённый возраст больше максимального ({PersonLibrary.MaxAge})!");
             Person.Age = InputDigit(PersonTemplate.AgeInputTemplate);
         }
 
+        // Ввод пола
+        //
         string GenderString = InputGender(PersonTemplate.GenderInputTemplate);
-        int GenderNumber = (int)GenderList.nd;
+        int GenderNumber = (int)GenderList.nd; // Объявление переменной GenderNumber для выбора пола в перечислении GenderList
 
-        switch (GenderString)
+        switch (GenderString) // выбор цифры на основе введённого в консоль символа 
         {
             case "м":
                 GenderNumber = 0;
@@ -260,30 +326,45 @@ public class Person
                 GenderNumber = 1;
                 break;
         }
-        Person.Gender = (GenderList)GenderNumber;
+        Person.Gender = (GenderList)GenderNumber; // задание пола персоны
         return Person;
     }
 
+    /// <summary>
+    /// Функция RegisterChanger для изменения регистра введённых фамилии или имени персоны
+    /// </summary>
+    /// <param name="InputString"></param>
+    /// <param name="Delimiters"></param>
+    /// <returns>
+    /// Возвращается строка с изменённым регистром фамилии или имени персоны
+    /// </returns>
     public static string RegisterChanger(string InputString,char[] Delimiters)
     {
-        string[] SplittedInputString = InputString.Split(Delimiters);
-        foreach (string s in SplittedInputString)
+        string[] SplittedInputString = InputString.Split(Delimiters); // Разбиение введённой строки на слова (в случае сложных имени или фамилии)
+        foreach (string s in SplittedInputString) // Для каждого слова в строке осуществляется изменение регистра
         {
-            string s0, s1 = s;
-            s0 = s.ToLower();
-            s0 = Char.ToUpper(s0[0]) + s0.Substring(1);
-            InputString = InputString.Replace(s1, s0);
+            string s0 = s;
+            s0 = s.ToLower(); // Перевод всего слова в нижний регистр
+            s0 = Char.ToUpper(s0[0]) + s0.Substring(1); // Перевод первого символа слова в верхний регистр
+            InputString = InputString.Replace(s, s0); // Замена слова в исходной строке
         }
         return InputString;
     }
 
+    /// <summary>
+    /// Функция GetRandomPerson для ввода случайной персоны
+    /// </summary>
+    /// <returns>
+    /// Возвращается случайная персона
+    /// </returns>
     public static Person GetRandomPerson()
     {
         Person Person = new Person();
-        Random RandomNumber = new Random();
-        Person.Age = RandomNumber.Next(PersonLibrary.MaxAge);
-        Person.Gender = (GenderList)RandomNumber.Next(0, 2);
-        switch (Person.Gender)
+        Random RandomNumber = new Random(); // создание экземппляра объекта класса Random для генерации случайных (псевдослучайных) чисел
+        Person.Age = RandomNumber.Next(PersonLibrary.MaxAge); // задание случайного возраста в диапазоне от 0 до максимально возможного возраста
+        Person.Gender = (GenderList)RandomNumber.Next(0, 2); // задание случайного пола
+
+        switch (Person.Gender) // выбор стандартных фамилии и имени на основе пола
         {
             case GenderList.мужской:
                 Person.Surname = PersonLibrary.StandardMaleSurnameLibrary[RandomNumber.Next(PersonLibrary.StandardMaleSurnameCount)];
@@ -298,26 +379,33 @@ public class Person
     }
 }
 
+/// <summary>
+/// Класс PersonLibrary - библиотека стандартных имён и фамилий
+/// </summary>
 public class PersonLibrary
 {
-    public static string[] StandardMaleSurnameLibrary = new string[] { "Иванов", "Петров", "Сидоров", "Сергеев" };
-    public static string[] StandardFemaleSurnameLibrary = new string[] { "Иванова", "Петрова", "Сидорова", "Сергеева" };
-    public static string[] StandardMaleNameLibrary = new string[] { "Иван", "Андрей", "Александр", "Константин", "Сергей", "Дмитрий" };
-    public static string[] StandardFemaleNameLibrary = new string[] { "Татьяна", "Светлана", "Наталья", "Александра", "Элла", "Дарья" };
+    public static string[] StandardMaleSurnameLibrary = new string[] { "Иванов", "Петров", "Сидоров", "Сергеев" }; // Мужские фамилии
+    public static string[] StandardFemaleSurnameLibrary = new string[] { "Иванова", "Петрова", "Сидорова", "Сергеева" }; // Женские фамилии
+    public static string[] StandardMaleNameLibrary = new string[] { "Иван", "Андрей", "Александр", "Константин", "Сергей", "Дмитрий" }; // Мужские имена
+    public static string[] StandardFemaleNameLibrary = new string[] { "Татьяна", "Светлана", "Наталья", "Александра", "Элла", "Дарья" }; // Женские имена
 
-    public static int StandardMaleNameCount = StandardMaleNameLibrary.Length;
-    public static int StandardMaleSurnameCount = StandardMaleSurnameLibrary.Length;
-    public static int StandardFemaleNameCount = StandardFemaleNameLibrary.Length;
-    public static int StandardFemaleSurnameCount = StandardFemaleSurnameLibrary.Length;
+    public static int StandardMaleNameCount = StandardMaleNameLibrary.Length; // Число стандартных мужских имён
+    public static int StandardMaleSurnameCount = StandardMaleSurnameLibrary.Length; // Число стандартных мужских фамилий
+    public static int StandardFemaleNameCount = StandardFemaleNameLibrary.Length; // Число стандартных женских имён
+    public static int StandardFemaleSurnameCount = StandardFemaleSurnameLibrary.Length; // Число стандартных женских фамилий
 
-    public static int MaxAge = 118;
+    public static int MaxAge = 118; // Максимально возможный возраст персоны
 }
+
+/// <summary>
+/// Класс PersonTemplate - библиотека шаблонов строк для вывода в консоль информации о персоне
+/// </summary>
 public class PersonTemplate
 {
     public static string SurnameInputTemplate = "Введите фамилию персоны: ";
     public static string NameInputTemplate = "Введите имя персоны: ";
     public static string AgeInputTemplate = "Введите возраст персоны: ";
-    public static string GenderInputTemplate = "Введите пол персоны (м - мужской, ж - женский): ";
+    public static string GenderInputTemplate = "Введите пол персоны (м/М/m/M - мужской, ж/Ж/f/F - женский): ";
 
     public static string SurnameOutputTemplate = "Фамилия: ";
     public static string NameOutputTemplate = "Имя: ";
